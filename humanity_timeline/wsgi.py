1648311e6a7b30e43e07c6747c20d569a01d07de
Chiragbisht <chiragbisht2003@gmail.com>
2024-10-19 03:12:42 +0530

    neww

--- humanity_timeline/wsgi.py
+++ humanity_timeline/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'humanity_timeline.settings')
 
 application = get_wsgi_application()
+app = application
